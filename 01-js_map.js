4b4c49f5b991bda46511399f4089050e637b017e
marcosantonastasi <marco.santonastasi@gmail.com>
2022-01-20 18:56:17 +0100

    done examples

--- 01-js_map.js
+++ 01-js_map.js
@@ -1,1 +1,46 @@
 // Class code examples
+
+const names =  ["Marco", "Carlos", "Carol"]
+
+const capitalise = (string)=>{
+    return string.toUpperCase()
+}
+
+const allCaps = names.map( capitalise ) //  ["Marco", "Carlos", "Carol"].map()
+
+console.log(
+    names,
+    allCaps
+    )
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const someCallbackFunction = ()=> {}
+const double = numbers.map( (element)=> { return element * 2} ) // numbers.map ( someCallbackFunction )
+
+console.log(double)
+
+const students = [
+    {name: 'Rafa', color: 'Blue', age: 12},
+    {name: 'Elise', color: 'Pink', age: 21},
+    {name: 'Bodei', color: 'Yellow', age: 31},
+    {name: 'Jorge', color: 'Red', age: 11}
+    ]
+
+const greetings = students.map( (student)=> {
+    return `Hello ${student.name}, you are ${student.age} years old and your fav color is ${student.color}`
+} )
+
+greetings.forEach( (el)=>console.log(el) )
+
+console.log(
+    ["Marco", "Carlos", "Carol"]
+    .map(()=>{})
+    .length
+)
+
+numbers
+  .map( (el)=>{return el * 2} )
+  .filter( (el)=>{return el < 13} )
+  .sort( (a,b)=>{return a - b} )
+
--- 02-js_reduce.js
+++ 02-js_reduce.js
@@ -1,1 +1,44 @@
 // Class code examples
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const callBack = (oldAccumulator, currentElement )=>{
+    newAccumulator =  oldAccumulator + currentElement
+    return newAccumulator  // MUST ALWAYS return the new accumulator
+}
+
+const sum = numbers.reduce(
+    callBack,
+    0 // Starting value of the accumulator is teh second parameter
+)
+
+console.log(sum)
+
+const students = [
+	{firstName: 'John', lastName: 'Carr', age: 34},
+  {firstName: 'Leonardo', lastName: 'Di Vittorio', age: 21},
+  {firstName: 'Jessy', lastName: "Mc Scrudge", age: 99},
+  {firstName: 'Lisa', lastName: "Parker", age: 22},
+  {firstName: 'Marco', lastName: "Santo", age: 48}
+]
+
+const countOlderThan30 = students.reduce(
+    (count, student)=>{
+        if(student.age > 30) return count+=1
+        else return count
+    },
+    0
+    )
+
+console.log(countOlderThan30)
+
+const classRoster = students.reduce(
+    (roster, student)=>{
+      roster.push(`${student.firstName} ${student.lastName}`)
+      return roster
+    }
+    ,
+    []
+)
+
+console.log("roster: ", classRoster)
